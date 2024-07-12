78fa8123d27154af108dca110a8e0178d77cb35b
Kalyankumar <kalyankumarpulapaka@gmail.com>
2024-07-13 00:33:32 +0530

    add

---
+++ index.js
@@ -0,0 +1,31 @@
+/** @format */
+
+import express from "express";
+import AppRouter from "./src/routers/router.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/", AppRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Server! 🌐");
+});
+
+mongoose
+  .connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
